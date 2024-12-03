66e985e36e35cd3a46940dad3ad9c07f0a217497
Ricardo Franco Pérez <rfrancop01@gmail.com>
2024-12-03 13:05:12 +0000

    [SP-91] feat: Corregido

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -2,61 +2,21 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg"
 
 
-export const Card = () => {
-    const cardTitle = "Card title"
-    const cardText = "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-    const cardButton = "Find Out More!"
+export const Card = (props) => {
+
+    const{ image, title, text } = props.Cards;
     
     return (
 
-        <div className="card-body row row-cols-1 row-cols-md-4 g-4 mt-1 mb-4">
-            <div className="col">
-                <div className="card h-100">
-                    <img src={rigoImage} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{cardTitle}</h5>
-                            <p className="card-text">{cardText}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" class="btn btn-primary">{cardButton}</a>
-                        </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card h-100">
-                    <img src={rigoImage} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{cardTitle}</h5>
-                            <p className="card-text">{cardText}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" class="btn btn-primary">{cardButton}</a>
-                        </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card h-100">
-                    <img src={rigoImage} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{cardTitle}</h5>
-                            <p className="card-text">{cardText}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" class="btn btn-primary">{cardButton}</a>
-                        </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card h-100">
-                    <img src={rigoImage} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{cardTitle}</h5>
-                            <p className="card-text">{cardText}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" class="btn btn-primary">{cardButton}</a>
-                        </div>
-                </div>
+        <div className="card h-100 mt-4">
+            <img src={image} className="card-img-top" style={{ height: '200px', objectFit: 'cover' }} alt={title} />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{text}</p>
+                <hr style={{ border: 'none', height: '1px', backgroundColor: '#343a40', margin: '0 -15px', marginBottom: '20px' }} />
+                <button className="btn btn-primary">
+                    Find out More!
+                </button>
             </div>
         </div>
     );
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -7,7 +7,31 @@ import { Jumbotron } from "./Jumbotron";
 import { Card } from "./Cards";
 import { Footer } from "./Footer";
 
-//create your first component
+const Cards = [
+	{
+		image: "https://picsum.photos/id/9/5000/3269",
+		title: "Set up minimalista",
+		text: "Vamos a escribir código",
+	},
+	{
+		image: "https://picsum.photos/id/84/1280/848",
+		title: "Puente",
+		text: "Puente colgante",
+
+
+	},
+	{
+		image: "https://picsum.photos/id/23/3887/4899",
+		title: "¿Comemos?",
+		text: "¿Tienes hambre?",
+	},
+	{
+		image: "https://picsum.photos/id/88/1280/1707",
+		title: "Ciudad con tráfico",
+		text: "Usa el transporte público",
+	}
+];
+
 const Home = () => {
 	return (
 		<div className="text-center">
@@ -16,8 +40,15 @@ const Home = () => {
 
 			<div className="container">
 				<Jumbotron />
-				<Card />
+				<div className="row">
+					{Cards.map((item, index) => (
+						<div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
+							<Card Cards={item} />
+						</div>
+					))}
+				</div>
 			</div>
+
 				<Footer />
 
 		</div>
